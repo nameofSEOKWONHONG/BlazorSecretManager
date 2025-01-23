49ee35e8df460c3dc3797e7a0ecf3b55519bc9a3
seokwonhong <h20913@gmail.com>
2025-01-23 20:59:43 +0900

    update

--- src/Infrastructure/CustomAuthStateProvider.cs
+++ src/Infrastructure/CustomAuthStateProvider.cs
@@ -107,20 +107,3 @@ public class CustomAuthStateProvider : AuthenticationStateProvider
     }
 }
 
-public class CustomCircuitHandler : CircuitHandler
-{
-    public bool IsPrerendering { get; private set; } = true;
-
-    public override Task OnConnectionUpAsync(Circuit circuit, CancellationToken cancellationToken)
-    {
-        // 클라이언트 연결이 이루어진 시점
-        IsPrerendering = false;
-        return Task.CompletedTask;
-    }
-
-    public override Task OnConnectionDownAsync(Circuit circuit, CancellationToken cancellationToken)
-    {
-        // 클라이언트 연결이 종료된 시점
-        return Task.CompletedTask;
-    }
-}
---
+++ src/Infrastructure/CustomCircuitHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Server.Circuits;
+
+namespace BlazorSecretManager.Infrastructure;
+
+public class CustomCircuitHandler : CircuitHandler
+{
+    public bool IsPrerendering { get; private set; } = true;
+
+    public override Task OnConnectionUpAsync(Circuit circuit, CancellationToken cancellationToken)
+    {
+        // 클라이언트 연결이 이루어진 시점
+        IsPrerendering = false;
+        return Task.CompletedTask;
+    }
+
+    public override Task OnConnectionDownAsync(Circuit circuit, CancellationToken cancellationToken)
+    {
+        // 클라이언트 연결이 종료된 시점
+        return Task.CompletedTask;
+    }
+}
